eae9dc402bce981026110283cd76875d0356710e
andchir@gmail.com <andchir@gmail.com>
2016-09-05 13:59:18 +0300

    First commit

--- assets/components/migx/js/mgr/migx.js
+++
@@ -1,9 +0,0 @@
-var Migx = function(config) {
-    config = config || {};
-    Migx.superclass.constructor.call(this,config);
-};
-Ext.extend(Migx,Ext.Component,{
-    page:{},window:{},grid:{},tree:{},panel:{},combo:{},config: {}
-});
-Ext.reg('migx',Migx);
-Migx = new Migx();
--- assets/components/migx/js/mgr/sections/index.js
+++
@@ -1,7 +0,0 @@
-Ext.onReady(function() {
-    MODx.load({ 
-		xtype: 'migx-page-home'
-        ,object_id: Migx.config.request.object_id
-		,configs: Migx.config.request.configs
-    });
-});
